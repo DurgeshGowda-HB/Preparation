9a9e46aa807ffb56e93510017f2f2d29d8014fb5
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-03-04 18:00:00 +0530

    updated

--- java/questions/others/A_Z.java
+++ java/questions/others/A_Z.java
@@ -3,6 +3,7 @@ package questions.others;
 public class A_Z {
 
     public static void main(String[] args) {
+        System.out.println("Printing alphabet");
         for(char c='a'; c<='z'; c++){
             System.out.print(c + " ");
         }
