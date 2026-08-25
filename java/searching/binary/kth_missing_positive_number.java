ae3b00da7c069dcc6501d2c12ff7f951929ab8df
Durgesh Gowda HB <durgeshgowda231@gmail.com>
2026-08-25 20:00:00 +0530

    find Kth missing positive number

--- java/searching/binary/kth_missing_positive_number.java
+++ java/searching/binary/kth_missing_positive_number.java
@@ -1,5 +1,30 @@
 package searching.binary;
 
 public class kth_missing_positive_number {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public int findKthPositive(int[] arr, int k) {
+
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left <= right){
+
+            int mid = left + (right - left) /2;
+
+            int missing = arr[mid] - mid - 1;
+
+            if(missing < k){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+
+        }
+        return left + k;
+    }
     
 }
