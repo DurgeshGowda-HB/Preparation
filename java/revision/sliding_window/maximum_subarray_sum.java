bcdb38f7a2246bef1cb2fb581e0990732b0365c7
Durgesh Gowda HB <117710393+durgesh-hb@users.noreply.github.com>
2026-09-02 13:14:01 +0530

    Add mini method for maximum subarray size calculation
    Added a new method 'mini' to calculate the maximum size of subarray with sum less than or equal to target. Updated comments for clarity and maintained the existing 'maxi' method for minimum size calculation.

--- java/revision/sliding_window/maximum_subarray_sum.java
+++ java/revision/sliding_window/maximum_subarray_sum.java
@@ -3,34 +3,71 @@ package revision.sliding_window;
 public class maximum_subarray_sum {
 
     public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 
-		int[] arr = {2,3,1,2,4,3};
-		int target = 7;
-		System.out.println(maxi(arr,target));
-		
-	}
-	
-	static int maxi(int[] arr, int target) {
-		
-		int start = 0;
-		int sum = 0;
-		int min_length = Integer.MAX_VALUE;
-		
-		for(int end = 0; end <arr.length; end++) {
-			
-			sum += arr[end];
-			
-			while(sum >= target) {
-				
-				min_length = Math.min(min_length, end - start + 1);
-				
-				sum -=arr[start];
-				start++;
-				
-			}
-			
-		}
-		return min_length;
-	}
+        int[] arr = {2, 3, 1, 2, 4, 3};
+        int target = 7;
+
+        System.out.println(maxi(arr, target));
+    }
+
+// 	MINIMUM SIZE
+// → condition: sum >= target
+// → need minimum length
+// → valid → update → shrink
+
+
+// MAXIMUM SIZE
+// → condition: sum <= target
+// → need maximum length
+// → invalid → shrink → valid → update
+
+
+// Both use sliding window.
+// Main difference = when we update.
+
+    static int maxi(int[] arr, int target) {
+
+        int start = 0;
+        int sum = 0;
+        int min_length = Integer.MAX_VALUE;
+
+        for (int end = 0; end < arr.length; end++) {
+
+            sum += arr[end];
+
+            while (sum >= target) {
+
+                min_length = Math.min(min_length, end - start + 1);
+
+                sum -= arr[start];
+                start++;
+            }
+        }
+
+        return min_length;
+    }
+
+    static int mini(int[] arr, int target) {
+
+        int start = 0;
+        int sum = 0;
+
+        int max_length = 0; // min_length → max_length
+
+        for (int end = 0; end < arr.length; end++) {
+
+            sum += arr[end];
+
+            while (sum > target) {
+
+                sum -= arr[start];
+                start++;
+            }
+
+            max_length = Math.max(max_length, end - start + 1);
+        }
+
+        return max_length; 
+    }
 }
